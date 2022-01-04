bae9acfae42dd7686ff7b6c3e866b0f95a84dab8
Lewin <lewinkoon+github@gmail.com>
2022-01-04 16:14:49 +0100

    Change variables name

--- day-01/main.go
+++ day-01/main.go
@@ -10,18 +10,17 @@ import (
 func main() {
 	data, _ := os.ReadFile("input.txt")
 
-	ss := strings.Split(string(data), "\n")
-	previous := -1
+	slices := strings.Split(string(data), "\n")
+	prev := -1
 	c := 0
-	for _, s := range ss {
+	for _, s := range slices {
 		n, _ := strconv.Atoi(s)
-		if previous != -1 {
-			if n > previous {
+		if prev != -1 {
+			if n > prev {
 				c++
 			}
 		}
-		previous = n
-		// fmt.Println(n)
+		prev = n
 	}
 
 	fmt.Print(c)
